da3b1b07f8de006755a9edf9779085ef39e2ba8f
cheolminJOO <cheolmin.joo@gmail.com>
2024-02-27 18:08:31 +0900

    240227 study complete !

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ChangeEvent, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [names, setNames] = useState(["주철민"]);
+  const [name , setName] = useState('')
+
+  const onChangeName = (e : ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value)
+  }
+
+  const onClickSubmitBtn = () => {
+    setNames((prev) => {
+      console.log("prev",prev)
+    return [name, ...prev]})
+  }
 
   return (
-    <>
+    <div>
+      <p>이름을 입력하시면 출력됩니다.</p>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input onChange={onChangeName} type='text' />
+        <button onClick={onClickSubmitBtn}>등록</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      {names.map((name) => (
+        <h1>{name}</h1>
+      ))}
+    </div>
+  );
+};
 
-export default App
+export default App;
